0dc880a738f07070dd8235a2cdf683030cb5c4d1
kayk <kayksantana133@gmail.com>
2026-08-07 19:49:46 -0300

    feat: add OpenAPI documentation with bearer auth

---
+++ src/main/java/com/lootsafe/swagger/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.lootsafe.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Lootsafe API", version = "v2", description = "Lootsafe Documentation"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+@Profile("!prod")
+public class SwaggerConfig {
+}
